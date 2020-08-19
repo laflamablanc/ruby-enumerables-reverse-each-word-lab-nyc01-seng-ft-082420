aa9d1927ed94706ebf34a64e2e6a46c77426dea1
Sean LaFlam <sean.laflam@gmail.com>
2020-08-19 01:51:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,18 +1,18 @@
-# def reverse_each_word(sentence)
-#   words = sentence.split
-#   new = words.collect do |word|
-#     word.reverse
-#   end
-#   new.join(" ")
-# end
-
 def reverse_each_word(sentence)
   words = sentence.split
-  new = []
-  words.each do |word|
-    new << word.reverse
+  new = words.collect do |word|
+    word.reverse
   end
   new.join(" ")
 end
+
+# def reverse_each_word(sentence)
+#   words = sentence.split
+#   new = []
+#   words.each do |word|
+#     new << word.reverse
+#   end
+#   new.join(" ")
+# end
     
   
